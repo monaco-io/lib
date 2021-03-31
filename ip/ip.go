b118e4145afb2dfe635b78b1f662d46b6e9c5d27
卢旭泽 <luxuze@icloud.com>
2021-03-31 17:20:50 +0800

    refactor external

--- ip/ip.go
+++ ip/ip.go
@@ -3,38 +3,30 @@ package ip
 import (
 	"fmt"
 	"net"
-	"strings"
 )
 
 // TODO tmp PIP
 // ExternalV4 ...
 func ExternalV4() (ips []string) {
-	inters, err := net.Interfaces()
+	addrs, err := net.InterfaceAddrs()
 	if err != nil {
 		return
 	}
-	for _, inter := range inters {
-		if !strings.HasPrefix(inter.Name, "lo") {
-			addrs, err := inter.Addrs()
-			if err != nil {
+
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok {
+			if ipnet.IP.IsLoopback() ||
+				ipnet.IP.IsLinkLocalMulticast() ||
+				ipnet.IP.IsLinkLocalUnicast() {
 				continue
 			}
-			for _, addr := range addrs {
-				if ipnet, ok := addr.(*net.IPNet); ok {
-					if ipnet.IP.IsLoopback() ||
-						ipnet.IP.IsLinkLocalMulticast() ||
-						ipnet.IP.IsLinkLocalUnicast() {
-						continue
-					}
-					if ip4 := ipnet.IP.To4(); ip4 != nil {
-						if (ip4[0] == 10) ||
-							(ip4[0] == 172 && ip4[1] >= 16 && ip4[1] <= 31) ||
-							(ip4[0] == 192 && ip4[1] == 168) {
-							continue
-						}
-						ips = append(ips, ipnet.IP.String())
-					}
+			if ip4 := ipnet.IP.To4(); ip4 != nil {
+				if (ip4[0] == 10) ||
+					(ip4[0] == 172 && ip4[1] >= 16 && ip4[1] <= 31) ||
+					(ip4[0] == 192 && ip4[1] == 168) {
+					continue
 				}
+				ips = append(ips, ipnet.IP.String())
 			}
 		}
 	}
