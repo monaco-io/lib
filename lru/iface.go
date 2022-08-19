08b948e964d33ab11a0c20f9a6632071fa9c8508
卢旭泽 <luxuze@icloud.com>
2022-08-19 11:32:35 +0800

    generic lru

--- lru/iface.go
+++ lru/iface.go
@@ -1,6 +1,6 @@
 package lru
 
-// LRUCache is the interface for simple LRU cache.
+// ICache is the interface for thread safe LRU cache.
 type ICache[K comparable, V any] interface {
 	// Adds a value to the cache, returns true if an eviction occurred and
 	// updates the "recently used"-ness of the key.
