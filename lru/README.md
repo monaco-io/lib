bf3b4699cd26a9b1a56d2e6d0471c0ae826472f2
卢旭泽 <luxuze@icloud.com>
2022-08-19 11:47:02 +0800

    generic lru

---
+++ lru/README.md
@@ -0,0 +1,55 @@
+# lib/lru
+
+thread safe LRU cache.
+
+```go
+// example
+package main
+
+import (
+	"log"
+	"math"
+	"time"
+
+	"github.com/monaco-io/lib/lru"
+	"golang.org/x/sync/errgroup"
+)
+
+var instance = lru.New[int, int](100, time.Second*5)
+
+func main() {
+	c1 := time.After(time.Second * 3)
+	c2 := time.After(time.Second * 10)
+	f1 := func() error {
+		for {
+			select {
+			case <-c1:
+				return nil
+			default:
+				for i := 0; i <= math.MaxInt8; i++ {
+					instance.Add(i, i)
+				}
+			}
+		}
+	}
+	f2 := func() error {
+		for {
+			select {
+			case <-c2:
+				return nil
+			default:
+				for i := 0; i <= math.MaxInt8; i++ {
+					v, ok := instance.Get(i)
+					log.Println(i, v, ok)
+				}
+			}
+		}
+	}
+	var eg errgroup.Group
+	eg.Go(f1)
+	eg.Go(f2)
+	eg.Wait()
+}
+
+
+```
--- lru/iface.go
+++ lru/iface.go
@@ -1,5 +1,13 @@
 package lru
 
+import (
+	"container/list"
+	"sync"
+	"time"
+
+	"github.com/monaco-io/lib/syncmap"
+)
+
 // ICache is the interface for thread safe LRU cache.
 type ICache[K comparable, V any] interface {
 	// Adds a value to the cache, returns true if an eviction occurred and
@@ -15,4 +23,24 @@ type ICache[K comparable, V any] interface {
 
 	Len() int
 	Clear()
+}
+
+// New creates a new Cache.
+// If maxEntries is zero, the cache has no limit and it's assumed
+// that eviction is done by the caller.
+func New[K comparable, V any](limit int, ttl time.Duration) *Cache[K, V] {
+	c := Cache[K, V]{
+		limit: defaultLength,
+		ttl:   defaultTTL,
+		cache: list.New(),
+		hash:  syncmap.New[K, *list.Element](),
+		lock:  new(sync.Mutex),
+	}
+	if limit != 0 {
+		c.limit = limit
+	}
+	if ttl != 0 {
+		c.ttl = ttl
+	}
+	return &c
 }
