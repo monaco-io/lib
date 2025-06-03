e3dc49e739269fc6acff5275dc3a9f30ea260c81
卢旭泽(luxz) <luxz@zenmen.com>
2025-06-03 10:37:03 +0800

    2025-06-03 10:37:03

--- syncmap/syncmap.go
+++ syncmap/syncmap.go
@@ -28,3 +28,17 @@ func (m Map[K, V]) Store(key K, value V) {
 func (m Map[K, V]) Delete(key K) {
 	m.syncmap.Delete(key)
 }
+
+func (m Map[K, V]) Range(f func(key K, value V) bool) {
+	m.syncmap.Range(func(key, value any) bool {
+		k, ok := key.(K)
+		if !ok {
+			return false
+		}
+		v, ok := value.(V)
+		if !ok {
+			return false
+		}
+		return f(k, v)
+	})
+}
