6ff0e8c229796959f4b1c741363e648747891995
卢旭泽 <luxuze@icloud.com>
2025-08-23 01:07:54 +0800

    2025-08-23 01:07:54

--- xjson/xjson.go
+++ xjson/xjson.go
@@ -1,47 +1,66 @@
 package xjson
 
-import "github.com/bytedance/sonic"
+import (
+	"encoding/json"
 
-var xjson = sonic.Config{
-	NoQuoteTextMarshaler:    true,
-	NoValidateJSONMarshaler: true,
-	NoValidateJSONSkip:      true,
-}.Froze()
+	"github.com/bytedance/sonic"
+)
+
+var useSonic bool
+
+func UseSonic() {
+	useSonic = true
+}
+
+func marshal(v any) ([]byte, error) {
+	if useSonic {
+		return sonic.Marshal(v)
+	}
+	return json.Marshal(v)
+}
+
+func unmarshal(data []byte, v any) error {
+	if useSonic {
+		return sonic.Unmarshal(data, v)
+	}
+	return json.Unmarshal(data, v)
+}
 
 func Marshal(v any) ([]byte, error) {
-	return xjson.Marshal(v)
+	return marshal(v)
 }
 
 func MarshalX(v any) []byte {
-	b, _ := xjson.Marshal(v)
+	b, _ := marshal(v)
 	return b
 }
 
 func MarshalStringX(v any) string {
-	b, _ := xjson.Marshal(v)
+	b, _ := marshal(v)
 	return string(b)
 }
 
 func MarshalString(v any) (string, error) {
-	return xjson.MarshalToString(v)
+	b, err := marshal(v)
+	return string(b), err
 }
 
 func Unmarshal(data []byte, v any) error {
-	return xjson.Unmarshal(data, v)
+	return unmarshal(data, v)
 }
 
 func UnmarshalString(str string, v any) error {
-	return xjson.UnmarshalFromString(str, v)
+	return unmarshal([]byte(str), v)
 }
 
 func UnmarshalT[T any](b []byte) (T, error) {
 	var v T
-	err := xjson.Unmarshal(b, &v)
+	err := unmarshal(b, &v)
 	return v, err
 }
 
 func UnmarshalStringT[T any](str string) (T, error) {
 	var v T
-	err := xjson.UnmarshalFromString(str, &v)
+	err := unmarshal([]byte(str), &v)
 	return v, err
 }
