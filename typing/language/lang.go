c895f290e77b21125915266995ed897c410ec9fb
卢旭泽 <luxuze@icloud.com>
2025-08-23 19:49:00 +0800

    2025-08-23 19:49:00

---
+++ typing/language/lang.go
@@ -0,0 +1,13 @@
+package lib
+
+type Language string
+
+const (
+	LangZhCN Language = "zh-cn"
+	LangZhTW Language = "zh-tw"
+	LangZhHK Language = "zh-hk"
+	LangEnUS Language = "en-us"
+	LangJaJP Language = "ja-jp"
+
+	LangDefault Language = LangZhCN
+)
